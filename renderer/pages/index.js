db7e72906d3d87c50d86402b13ea4dbf9197e072
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2024-02-29 10:46:46 +0000

    Add file system logic

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,13 +1,21 @@
-import { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react'
+import { filePath } from '../utils/config'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    useEffect(() => {
-        // on mount
-    }, []);
-    return (
-        <div className={styles.wrapper}>
-            <h1>Nextron Boilerplate</h1>
-        </div>
-    );
+  useEffect(() => {
+    // on mount
+  }, [])
+  return (
+    <div className={styles.wrapper}>
+      <h1>Edit the file path in config.js to show the video below</h1>
+      <video
+        src={`media-loader:///${filePath}/idle.mp4`}
+        className={styles.video}
+        autoPlay
+        loop
+        muted
+      ></video>
+    </div>
+  )
 }
---
+++ renderer/utils/config.js
@@ -0,0 +1,1 @@
+export const filePath = 'C:/Users/ccgre/Documents/voxel/Videos'
